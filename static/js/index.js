179e981157bfd72d5a0d26723285b9aa136942f0
Cathy Chen <cathychenn@gmail.com>
2017-07-29 13:29:58 -0700

    js structure

---
+++ static/js/index.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom'
+
+// class Point extends React.Component {
+//   render() {
+//     return (
+//       <div className="point">
+//         <h1>Point {this.props.name}</h1>
+//       </div>
+//     );
+//   }
+// }
