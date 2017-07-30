9d27ea0fd9884dc2d17797cd86cb9c011854b4bc
Ben Sandler <ben1sandler@gmail.com>
2017-07-30 00:56:00 -0700

    fix streaming

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def check_voice():
     while True:
         subprocess.call('rm recording.wav', shell=True)
         subprocess.call('rm recording_final.wav', shell=True)
-        subprocess.call('sox -d recording.wav trim 0 4', shell=True)
+        subprocess.call('sox -d recording.wav trim 0 5', shell=True)
         subprocess.call('ffmpeg -i recording.wav -acodec pcm_s16le -ac 1 -ar 16000 recording_final.wav', shell=True)
         with open('recording_final.wav', 'rb') as f:
             r1 = requests.post(
@@ -37,9 +37,7 @@ def check_voice():
             query_url = r1.headers['Operation-Location']
             r2 = None
 
-            count = 0
             while r2 is None or r2.json()['status'] != 'succeeded':
-                count += 1
                 time.sleep(1.5)
 
                 r2 = requests.get(
@@ -50,12 +48,13 @@ def check_voice():
             speaker_id = r2.json()['processingResult']['identifiedProfileId']
             if speaker_id == speaker_zero_id:
                 current_speaker = 0
+                print '****** speaker is ben ******'
             elif speaker_id == speaker_one_id:
+                print '****** speaker is cathy ******'
                 current_speaker = 1
             else:
-                print 'neither speaker identified - keeping speaker as {}'.format(current_speaker)
+                print '****** neither speaker identified - keeping speaker as {} ******'.format(current_speaker)
 
 if __name__ == '__main__':
     threading.Thread(target=check_voice).start()
-    check_voice()
-    app.run()
+    app.run(debug=True, use_reloader=False)
