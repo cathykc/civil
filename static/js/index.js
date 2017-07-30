9753cffd6e712c6606e95fdd89ddff6256b580f0
Cathy Chen <cathychenn@gmail.com>
2017-07-29 17:54:44 -0700

    basic front

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Conversation from './conversation';
+import RecordButton from './record-button';
 
 ReactDOM.render(<Conversation/>, document.getElementById('conversation'));
+ReactDOM.render(<RecordButton/>, document.getElementById('record'));
--- static/js/point.js
+++ static/js/point.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Panel } from 'react-bootstrap';
 
 class Point extends React.Component {
 	render() {
 		var pointClass = "point-container point-level-" + this.props.level;
 		return (
-			<div className={ pointClass }>
-				<h1>Speaker: { this.props.speaker_id }</h1>
-				<div className="point-content">Content: { this.props.content }</div>
-			</div>
+			<Panel>
+				<div className={ pointClass }>
+					<div className='point-speaker'>Speaker: { this.props.speaker_id }</div>
+					<div className="point-content">Content: { this.props.content }</div>
+				</div>
+			</Panel>
 			);
 	}
 }
